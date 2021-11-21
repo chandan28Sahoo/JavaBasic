99023c4957b6f37711f695c3e1b72c4b6fb44364
chandan sahoo <chandan19@navgurukul.org>
2021-11-21 11:50:15 +0530

    commit for basics

---
+++ javaBasics/src/javaBasics/HelloJava.java
@@ -0,0 +1,99 @@
+package javaBasics;
+import java.util.Arrays;
+import java.util.*;
+public class HelloJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+			System.out.print("hello world\n");
+		// variables
+			String name = "chandan";
+			String clas = "1st year";
+			int age = 25;
+			System.out.print(name+clas+age);
+		*/
+		/*type - primitive type*/
+		// byte - 1 [-128 to 127]
+		//		short - 2
+		//		int - 4
+		//		long - 8
+		//		float - 4
+		//		double - 8
+		//		char - 2
+		//		boolean - 1 
+		//		byte age = 30;
+		//		int phone = 125689456;
+		//		long phone2 = 12568945689L;
+		//		float pi = 3.14F;
+		//		char letter = 'a';
+		//		boolean isAdult = true;
+		/*non-primitive*/
+		/*string*/
+		//		String name = new String("chandan");
+		//		String friend = "subho";
+		//		System.out.println(name + " " + friend);
+		//		System.out.print(friend.charAt(0));
+		//		String name2 = friend.replace("u","O");
+		//		System.out.print(name2);
+		/*array*/
+		//		int age = 30;
+		//		int physics = 65;
+		//		int chem = 63;
+		//		int eng = 50;
+		//		
+		//		int[] marks = new int[3];
+		//		marks[0] = physics;
+		//		marks[1] = chem;
+		//		marks[2] = eng;
+		//		System.out.println(marks[0]);
+		//		String[] array = new String[] {"John", "Mary", "Bob"};
+		//		System.out.println(Arrays.toString(array));
+		//		
+		//		int[] intArray = { 7, 9, 5, 1, 3 };
+		//		System.out.println(Arrays.toString(intArray));
+		//		System.out.println(intArray[0]);
+		//		Arrays.sort(intArray);
+		//		System.out.println(intArray[0]);
+		/*two dimensional array*/
+		//		int[][] marks = {{8,5,6,5},{10,25,14,52}};
+		//		System.out.println(Arrays.toString(marks[0]));
+		/*input*/
+		//		Scanner sc = new Scanner(System.in);
+		//		System.out.print("Input your age :\n");
+		//		String age = sc.nextLine();
+		//		System.out.println("your age is " + age);
+		
+		
+		/*comparison operator*/
+		
+		/*
+		 * a==b
+		 * a != b
+		 * a > b
+		 * a < b
+		 * a >= b
+		 * a <= b
+		 * 
+		 * */
+		
+		
+		/*condition if else*/
+		//		int age = 30;
+		//		if(age <= 70) {
+		//			System.out.print("yes");
+		//		}else {
+		//			System.out.print("no");	
+		//		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
