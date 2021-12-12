d783d5997517e42b8490c1142b95f9b1fa33f4e1
chandan sahoo <chandan19@navgurukul.org>
2021-12-12 12:05:32 +0530

    fix changes

--- JavaPrograms/src/JavaPrograms/FindDuplicate_ifExist_InArray.java
+++ JavaPrograms/src/JavaPrograms/FindDuplicate_ifExist_InArray.java
@@ -4,25 +4,35 @@ import java.util.Scanner;
 
 public class FindDuplicate_ifExist_InArray {
 
-	
+	public boolean checkChar(char arr[], char ab) {
+		boolean flag = false;
+		for(int j=0; j<=arr.length -1; j++) {
+			if(arr[j] == ab) {
+				System.err.print("char exist :");
+				flag = true;
+				break;
+			}
+		}
+		return flag;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		FindDuplicate_ifExist_InArray ob = new FindDuplicate_ifExist_InArray();
 		Scanner sc = new Scanner(System.in);
-		
 		char arr[] = new char[10];
 		for(int i=0; i<= arr.length -1; i++ ) {
-			System.out.println("Please enter the char  :");
+			System.out.println("Enter your char  :");
 			char ab = sc.next().charAt(0);
-			for(int j=0; j<=arr.length -1; j++) {
-				if(arr[j] == ab) {
-					System.err.println("char exist :");
-				}else {
-					arr[i] = ab;
-				}
-			}			
+			boolean check =  ob.checkChar(arr, ab);
+			
+			if(!check) {
+				arr[i] = ab;
+			}else {
+				i = i - 1;
+			}
 		}
-		for(int ele = 0; ele<=arr.length -1; ele++) {
-			System.out.println(arr[ele]);
+		for(int j=0; j<=arr.length -1; j++) {
+			System.out.println(arr[j]);
 		}
 	}
 
