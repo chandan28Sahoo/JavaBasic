61e9cbfaa6f3e27fc4805b954cef9540d45efe87
chandan28Sahoo <chandan19@navgurukul.org>
2022-02-13 13:32:18 +0530

    second commit

--- JavaPrograms/src/oops/Interfaces.java
+++ JavaPrograms/src/oops/Interfaces.java
@@ -1,6 +1,7 @@
 package oops;
 
 interface Animals{ // we can make contractor of interface
+	int leg = 10;
 	 void walk(); // this is called abstract function
 }
 
@@ -10,14 +11,15 @@ interface harbivore{
 class Horse implements Animals, harbivore {
 
 	public void walk() {
-		 
+		 System.out.println("animal walk with legs of " + leg);
 	}
 }
 public class Interfaces { // pure abstraction
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Animals h = new Horse();
+		h.walk();
 	}
 
 }
